943ff6ec55efe86860ca1d708a81ecc18529b1f7
CGLkousei <c5624208@aoyama.jp>
2024-12-15 09:15:55 +0900

    出力テスト

--- src/main/java/com/example/web01/CustomerController.java
+++ src/main/java/com/example/web01/CustomerController.java
@@ -1,14 +1,29 @@
 package com.example.web01;
 
 import com.example.web01.Class.Customer;
+import com.example.web01.Entity.CategoryEntity;
+import com.example.web01.Entity.DishEntity;
+import com.example.web01.Entity.RestaurantEntity;
+import com.example.web01.Service.CategoryService;
+import com.example.web01.Service.DishService;
+import com.example.web01.Service.RestaurantService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.List;
+
 @Controller
 public class CustomerController {
+    @Autowired
+    private RestaurantService restaurantService;
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private DishService dishService;
 
     @GetMapping("/customer/launch")
     public String launchCustomer(){
@@ -16,8 +31,11 @@ public class CustomerController {
     }
     @PostMapping("/customer/home")
     public String displayCustomerHome(Model model){
-        Customer c = new Customer();
-        model.addAttribute("customer", c);
+        List<RestaurantEntity> restaurants = restaurantService.getAllRestaurants();
+        List<CategoryEntity> categories = categoryService.getAllCategories();
+        List<DishEntity> dishes = dishService.getAllDishes();
+
+        model.addAttribute("restaurants", restaurants);
 
         return "customer/customerHome";
     }
